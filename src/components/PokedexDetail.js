7b7c0e214efe88595259be77aa389ae0d47f4ab6
Israel Quisbert <capricos999@gmail.com>
2022-07-27 20:57:13 -0300

    segunda

--- src/components/PokedexDetail.js
+++ src/components/PokedexDetail.js
@@ -5,7 +5,7 @@ import { useParams } from 'react-router-dom';
 const PokedexDetail = ({characterUrl}) => {
 
     const [character, setCharacter] = useState({})
-    const [movements, setMovements] = useState([])
+    const [movements, setMovements] = useState({})
 
     const { id } = useParams()
 
